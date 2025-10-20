de8f90cfbe98c3e324b4de5d600e5b4ecc637059
Kedia12 <kediaihogoza01@gmail.com>
2025-10-20 15:04:18 +0000

    feat: a program that prints the alphabet in lowercase, and then in uppercase.

--- 3-print_alphabets.c
+++ 3-print_alphabets.c
@@ -10,11 +10,11 @@
 int main(void)
 {
 char c;
-char d;
+
 for (c = 'a'; c <= 'z'; c++)
 putchar(c);
-for (d = 'A'; d <= 'Z'; d++)
-putchar(d);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
 putchar('\n');
 return (0);
 }
