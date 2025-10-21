5528ade2c88f2247edf7996c1369057c7d9f1d3e
Kedia12 <kediaihogoza01@gmail.com>
2025-10-21 14:27:18 +0000

    fix: removed errors for function

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * _isalpha - checks for an alphabetic character
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
+ */
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
