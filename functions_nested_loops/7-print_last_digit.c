11ede216fbd60b48852c56330ffb909c151ff941
Kedia12 <kediaihogoza01@gmail.com>
2025-10-22 09:20:11 +0000

    feat: a function that prints the last digit of a number.

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: number to check
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+int c;
+c = n % 10;
+if (c < 0)
+c = - c;
+_putchar(c + '0');
+return (c);
+}
