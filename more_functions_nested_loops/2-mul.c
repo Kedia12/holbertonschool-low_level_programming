fba742994b9891a6b1527b89035167d6994fa8df
Kedia12 <kediaihogoza01@gmail.com>
2025-10-27 09:47:48 +0000

    feat: a function that multiplies two integers.

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Result of multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
