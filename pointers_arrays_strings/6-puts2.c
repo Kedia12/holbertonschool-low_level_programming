5c60e9912daf54051d6773c2d8dc4915dd057dab
Kedia12 <kediaihogoza01@gmail.com>
2025-10-28 10:29:42 +0000

    feat: a function that prints every other character of a string

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * puts2 - a function that prints every other character of a string
+ * puts2 - prints every other character of a string
  * @str: a pointer to the string
- *
  */
 void puts2(char *str)
 {
