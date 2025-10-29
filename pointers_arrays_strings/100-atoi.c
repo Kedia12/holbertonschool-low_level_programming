c0090ea5d07180390574d6286c8a7e07d669a7e8
Kedia12 <kediaihogoza01@gmail.com>
2025-10-29 11:14:16 +0000

    fix : removed the space in between the operator and the variable

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
  * _atoi - converts a string to an integer
  * @s: input string
@@ -24,8 +25,13 @@ i++;
 }
 while (s[i] >= '0' && s[i] <= '9')
 {
-num = num * 10 + (s[i] - '0');
+int digit = s[i] - '0';
+if (sign == 1 && (num > (INT_MAX - digit) / 10))
+return (INT_MAX);
+if (sign == -1 && (num > (-(INT_MIN + digit)) / 10))
+return (INT_MIN);
+num = num * 10 + digit;
 i++;
 }
-return (num * sign);
+return (num *sign);
 }
