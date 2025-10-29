379ebcbb10cfd32affe124641ed9d78caaa35cd1
Kedia12 <kediaihogoza01@gmail.com>
2025-10-29 09:25:05 +0000

    feat: a function that prints every other character of a string, starting with the first character.

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,16 @@
 #include "main.h"
 /**
  * puts2 - prints every other character of a string
- * @str: a pointer to the string
+ * @str: pointer to the string
  */
 void puts2(char *str)
 {
-int i;
-for (i = 0; str[i] != '\0'; i += 2)
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
 _putchar(str[i]);
+i++;
+}
 _putchar('\n');
 }
