981e3b219d5f716cca830f2109cf835894084b52
Kedia12 <kediaihogoza01@gmail.com>
2025-10-31 14:23:47 +0000

    fix: betty style

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _atoi - converts a string to an integer
- * @s: the string to convert
- *
- * Return: the integer value of the string, or 0 if none
- */
+* _atoi - converts a string to an integer
+* @s: the string to convert
+*
+* Return: the integer value of the string, or 0 if none
+*/
 int _atoi(char *s)
 {
 int sign = 1;
