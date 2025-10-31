004b34863e8143595f09df9912ea17cfd8c4e6b0
Kedia12 <kediaihogoza01@gmail.com>
2025-10-31 14:13:51 +0000

    fix: betty style

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * _atoi - convert a string to an integer
  *
@@ -8,36 +7,31 @@
  *
  * Return: result is final integer
  */
-
 int _atoi(char *s)
 {
-	int index = 0;
-	int result = 0;
-	int number_neg = 0;
-	int sign = 0;
-
-	while (s[index] != '\0')
-	{
-		if (s[index] == '-')
-		{
-			number_neg += 1;
-		}
-
-		if (s[index] >= '0' && s[index] <= '9')
-		{
-			result = result * 10 + s[index] - '0';
-		}
-		index++;
-	}
-
-	if (number_neg % 2 == 0)
-	{
-		sign = 1;
-	}
-	else
-	{
-		sign = -1;
-	}
-
-	return (result * sign);
+int index = 0;
+int result = 0;
+int number_neg = 0;
+int sign = 0;
+while (s[index] != '\0')
+{
+if (s[index] == '-')
+{
+number_neg += 1;
+}
+if (s[index] >= '0' && s[index] <= '9')
+{
+result = result * 10 + s[index] - '0';
+}
+index++;
+}
+if (number_neg % 2 == 0)
+{
+sign = 1;
+}
+else
+{
+sign = -1;
+}
+return (result * sign);
 }
