eedacc978cfbb4ee5b7cc8adfcb4447083a19d8a
Kedia12 <kediaihogoza01@gmail.com>
2025-11-18 09:48:45 +0000

    fix: reoved extra if's

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -24,19 +24,14 @@ if (argc != 4)
 printf("Error\n");
 exit(98);
 }
-if (argv[2][1] != '\0')
-{
-printf("Error\n");
-exit(99);
-}
-op = get_op_func(argv[2]);
-if (op == NULL)
-{
-printf("Error\n");
-exit(99);
-}
 a = atoi(argv[1]);
 b = atoi(argv[3]);
+op = get_op_func(argv[2]);
+if (op == NULL || argv[2][1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
 if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 {
 printf("Error\n");
