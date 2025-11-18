516a673ea8f8d6e6ad8487229d7bf92b7c473b6c
Kedia12 <kediaihogoza01@gmail.com>
2025-11-18 10:50:07 +0000

    fix: added braces

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,9 +14,11 @@ va_list args;
 unsigned int i;
 va_start(args, n);
 for (i = 0; i < n; i++)
+{
 printf("%d", va_arg(args, int));
 if (separator != NULL && i < n - 1)
 printf("%s", separator);
+}
 va_end(args);
 printf("\n");
 }
