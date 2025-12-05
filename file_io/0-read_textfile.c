10df3767a4680b2ae7f3ca19129a92e092cd4699
Kedia12 <kediaihogoza01@gmail.com>
2025-12-05 09:30:11 +0000

    feat: a function that reads a text file and prints it to the POSIX standard output.

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout
+ * @filename: The name of the file
+ * @letters: Number of letters it should read and print
+ *
+ * Return: The actual number of letters it could read and print
+ *         0 if file can't be opened, read, or written, or on error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+ssize_t bytes_read, bytes_written;
+char *buffer;
+if (filename == NULL || letters == 0)
+return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+free(buffer);
+return (0);
+}
+bytes_read = read(fd, buffer, letters);
+if (bytes_read == -1)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+free(buffer);
+close(fd);
+return (0);
+}
+free(buffer);
+close(fd);
+return (bytes_written);
+}
